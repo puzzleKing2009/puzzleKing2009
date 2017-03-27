8c6cb9bcb213ad99ee51bb1943da2b24eb932e47
sneekxy <sneekxy@gmail.com>
2017-03-27 12:26:01 -0500

    Update pie.js

--- pie.js
+++ pie.js
@@ -4,7 +4,7 @@
 			$('#buy9').css('background-image','url(art/build9.1.png)');
 		}
 		if(player.pieGainEnable){
-			var pieWorth = 1+(Math.random()*314)/100;
+			var pieWorth = 1+(Math.random()*628)/100;
 			addMoney_1(pieWorth*buildings[8].currGain);
 			$('#pieRanGive').text(Math.round((pieWorth-1)*100).formatMoney());
 		}
